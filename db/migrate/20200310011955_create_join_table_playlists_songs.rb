bc32a1228ad3278430fd4aeead05cebfff192ef0
Stefanie <stefcodes@gmail.com>
2020-03-13 11:25:10 -0700

    create join table migration

--- db/migrate/20200310011955_create_join_table_playlists_songs.rb
+++ db/migrate/20200310011955_create_join_table_playlists_songs.rb
@@ -2,6 +2,7 @@ class CreateJoinTablePlaylistsSongs < ActiveRecord::Migration[6.0]
   def change
     create_join_table :playlists, :songs do |t|
        t.index [:playlist_id, :song_id]
+
     end
   end
 end
